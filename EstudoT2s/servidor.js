7a07a8262f775363cd924496f365fc2b9a7304f7
Diego Dias <157524759+diiego13@users.noreply.github.com>
2024-03-15 15:06:25 -0300

    Add files via upload

---
+++ EstudoT2s/servidor.js
@@ -0,0 +1,8 @@
+// Exemplo simples de script Java com Rhino Nashorn
+function executaLogicaServidor() {
+    var dataAtual = new java.util.Date();
+    print('Lógica do servidor executada em: ' + dataAtual);
+}
+
+executaLogicaServidor();
+
